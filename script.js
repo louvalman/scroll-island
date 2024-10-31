9485813d149f7d2ae445d05917da1b3d2301aea6
louvalman <louisdyrhauge@hotmail.com>
2024-10-31 22:45:59 +0100

    scroll island index

--- script.js
+++ script.js
@@ -2,12 +2,5 @@ const progress = document.querySelector('.progress');
 const links = document.querySelector('.links');
 
 progress.addEventListener('click', () => {
-  console.log('Progress clicked!');
   progress.classList.toggle('on');
-
-  if (progress.classList.contains('on')) {
-    links.style.display = 'flex'; // Show links
-  } else {
-    links.style.display = 'none'; // Hide links if 'on' class is removed
-  }
 });
