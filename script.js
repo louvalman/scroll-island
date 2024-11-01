7d58f842cfce67fb08ef14bc6d335f7a74eb5f15
louvalman <louisdyrhauge@hotmail.com>
2024-11-01 23:17:52 +0100

    make display absolute position to fix transition and implement scroll position percentage

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 const progress = document.querySelector('.progress');
 const links = document.querySelector('.links');
+const percentElement = document.querySelector('.percentage');
 
 progress.addEventListener('click', () => {
   progress.classList.toggle('on');
 });
+
+window.addEventListener('click', (event) => {
+  if (progress.classList.contains('on') && !progress.contains(event.target)) {
+    progress.classList.remove('on');
+  }
+});
+
+window.onscroll = () => {
+  const totalHeight = document.body.scrollHeight - window.innerHeight;
+  const y = window.scrollY;
+  const res = ((y / totalHeight) * 100).toFixed(0);
+  percentElement.textContent = `${Math.min(Number(res), 100)}%`;
+  /*   progressEl.style.setProperty("--t", `${Number(res) / 100}turn`); */
+};
